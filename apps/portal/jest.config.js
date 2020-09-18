e5018b2d469edbc2ca79a4af5f0c5b68d9302e6d
Alfredo Perez <aperez@octolabs.io>
2020-09-18 14:03:04 -0500

    adds project

---
+++ apps/portal/jest.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  name: 'portal',
+  preset: '../../jest.config.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: [
+        'jest-preset-angular/build/InlineFilesTransformer',
+        'jest-preset-angular/build/StripStylesTransformer',
+      ],
+    },
+  },
+  coverageDirectory: '../../coverage/apps/portal',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js',
+  ],
+  reporters: [
+    "default",
+    ["jest-html-reporters", {
+      "publicPath": "./jest-report/apps/portal",
+      "filename": "report.html",
+      "pageTitle": 'Portal Tests Report',
+      "expand": true
+    }]
+  ]
+};
---
+++ libs/ui/jest.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  name: 'ui',
+  preset: '../../jest.config.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: [
+        'jest-preset-angular/build/InlineFilesTransformer',
+        'jest-preset-angular/build/StripStylesTransformer',
+      ],
+    },
+  },
+  coverageDirectory: '../../coverage/libs/ui',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js',
+  ],
+  reporters: [
+    "default",
+    ["jest-html-reporters", {
+      "publicPath": "./jest-report/libs/ui",
+      "filename": "report.html",
+      "pageTitle": 'UI Libs Report',
+      "expand": true
+    }]
+  ]
+};
